78491dfb964b6424ffa5470e5783ffbf20582678
Torppa Tuomo I <totutotu@dat-8.cs.helsinki.fi>
2016-09-19 18:06:05 +0300

    ensimmäinen testi lisätty

--- luomuspyha/pyha/tests.py
+++ luomuspyha/pyha/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import unittest
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_not_logged_in_gets_redirected(self):
+    	response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, 'https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?target=KE.541&next')
