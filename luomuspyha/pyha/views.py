6a06942aea80336431509812c9753bb4243aa212
ninakatila <nkatila@gmail.com>
2016-09-21 16:59:12 +0300

    esimerkin pohjalta aloitussivulle asiakkaan raajaman targetin ja collectionin läpikäynti

--- luomuspyha/pyha/views.py
+++ luomuspyha/pyha/views.py
@@ -11,10 +11,10 @@ from pyha.login import log_out
 
 @require_http_methods(["GET"])
 def index(request):
-        if not "user_id" in request.session:
-                return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next')
-        context = {"title": "Tervetuloa " + request.session["user_name"] , "message": "pyhaton salaista tekstia juuri sinulle"}
-        return render(request, 'pyha/index.html', context)
+	context = {"title": "Tervetuloa " + request.session["user_name"] , "message": "pyhaton salaista tekstia juuri sinulle", "target_header":"Valitsemasi target: ", "targets": ['linnut','nisakkaat'], "collection_header":"Valitsemasi aineisto: ", "collections": ['ain1', 'ain2', 'ain3']}
+	if not "user_id" in request.session:
+                return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next&allowUnapproved=true')
+	return render(request, 'pyha/index.html', context)
 
 def login(request):      
 	return _process_auth_response(request)
@@ -27,14 +27,17 @@ def logout(request):
 
 def _process_auth_response(request):
     if not "token" in request.POST:
-        return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next')
+        return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next&allowUnapproved=true')
     if authenticate(request, request.POST["token"]):
-        return HttpResponseRedirect('../pyha/index')
+        return HttpResponseRedirect('pyha/index')
     else:
         return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next')
 
 def receiver(request):
-        #tassa kasitellaan putkeen tuotu json
+
         print(request)
+        #data = json.loads(request.body)
+        #filters = data['filters']
+        #collections = data ['collections']
         return HttpResponse('')
 	
