5cae7f82e8e9a77cc45e2a1785edf3a038632887
Anu Salminen <aesalmin@gmail.com>
2016-09-23 08:21:41 +0300

    model hahmotelmaa

--- luomuspyha/pyha/models.py
+++ luomuspyha/pyha/models.py
@@ -1,5 +1,28 @@
-from __future__ import unicode_literals
+﻿from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+Class Collection(models.Model):
+	collection_name = models.CharField(max_length=200)
+	collection_id = models.CharField(max_length=100)
+	collection_count = models.IntegerField()
+	# collection_contact
+	# collection_license
+
+	def __str__(self):
+		return self.collection_name
+
+Class Filter(models.Model):
+	target_list = []
+	time_list = []
+	place_list = []
+
+Class Request(models.Model):
+	request_id = models.CharField(max_length=100)
+	request_source = models.CharField(max_length=10)
+	request_email = models.CharField(max_length=100)
+	approximateMatches = models.IntegerField()
+	filter_list = []
+	collection_list = []
+	
