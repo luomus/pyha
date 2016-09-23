657de7c7f3e3d69c33e614999f21213b9040e0e8
Anu Salminen <aesalmin@gmail.com>
2016-09-23 16:09:15 +0300

    lisayksia modeliin

--- luomuspyha/pyha/models.py
+++ luomuspyha/pyha/models.py
@@ -9,18 +9,23 @@ class Collection(models.Model):
 	collection_count = models.IntegerField()
 	request = models.ForeignKey('Request', on_delete=models.CASCADE)
 	# collection_contact
-	# collection_license
+	# collection_license 
 
 	def __str__(self):
 		return self.collection_name
 
 class Request(models.Model):
 	request_id = models.CharField(max_length=100, primary_key=True)
+	request_date = models.DateTimeField()
 	request_source = models.CharField(max_length=10)
 	request_email = models.CharField(max_length=100)
 	approximateMatches = models.IntegerField()
 	filter_list = models.CharField(max_length=1000)
 	request_status = models.CharField(max_length=100)
+	# request_reason = models.CharField(max_length=1000)
+	# request_decision = models.CharField(max_length=100)
+	# request_decision_date = models.DateTimeField()
+	# request_decision_reason = models.CharField(max_length=1000)	
 
 	def __str__(self):
 		return self.request_id
