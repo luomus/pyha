72ab7b837a97be0f98fe137d0e5ba70e24f70f19
Anu Salminen <aesalmin@gmail.com>
2016-09-23 15:16:37 +0300

    model uusittu

--- luomuspyha/pyha/models.py
+++ luomuspyha/pyha/models.py
@@ -7,23 +7,20 @@ class Collection(models.Model):
 	collection_name = models.CharField(max_length=200)
 	collection_id = models.CharField(max_length=100)
 	collection_count = models.IntegerField()
+	request = models.ForeignKey('Request', on_delete=models.CASCADE)
 	# collection_contact
 	# collection_license
 
 	def __str__(self):
 		return self.collection_name
 
-class Filter(models.Model):
-	target_list = []
-	time_list = []
-	place_list = []
-
 class Request(models.Model):
-	request_id = models.CharField(max_length=100)
+	request_id = models.CharField(max_length=100, primary_key=True)
 	request_source = models.CharField(max_length=10)
 	request_email = models.CharField(max_length=100)
 	approximateMatches = models.IntegerField()
-	filter_list = []
-	collection_list = []
-	request_status = []
-	
+	filter_list = models.CharField(max_length=1000)
+	request_status = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.request_id
