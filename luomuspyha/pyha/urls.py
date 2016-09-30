b8568d5f88737f702c8397491a668f60aba0c881
aalri <alrial@dat-5.cs.helsinki.fi>
2016-09-30 11:58:30 +0300

    paivitys

--- luomuspyha/pyha/urls.py
+++ luomuspyha/pyha/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'pyha'
 urlpatterns = [
-    url(r'^/?$', views.login, name='login'),
+    url(r'^/?$', views.index, name='index'),
     url(r'^login/?$', views.login, name='login'),
     url(r'^logout/?$', views.logout, name='logout'),
     url(r'^index/?$', views.index, name='index'),
--- luomuspyha/pyha/views.py
+++ luomuspyha/pyha/views.py
@@ -12,21 +12,20 @@ from pyha.login import log_out
 from pyha.warehouse import store
 from pyha.models import Collection, Request
 
-@require_http_methods(["GET"])
 def index(request):	
 		if not logged_in(request):
-			return _process_auth_response(request)
+			return _process_auth_response(request,'/pyha/')
 		userEmail = request.session["user_email"]
 		request_list = Request.requests.filter(email=userEmail)
 		context = {"title": "Tervetuloa " + request.session["user_name"] , "message": "Kaytat sahkopostiosoitetta: " + request.session["user_email"], "requests": request_list }
 		return render(request, 'pyha/index.html', context)
 
 def login(request):      
-		return _process_auth_response(request, "")
+		return _process_auth_response(request, '/pyha/')
 
 def logout(request):
 		if not logged_in(request):
-			return _process_auth_response(request, "")
+			return _process_auth_response(request, '/pyha/')
 		context = {"title": "Kirjaudu ulos", "message": "Kirjauduit ulos onnistuneesti"}
 		log_out(request)
 		return render(request, 'pyha/index.html', context)
@@ -40,7 +39,7 @@ def _process_auth_response(request, indexpath):
 		if not "token" in request.POST:
 			return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next='+str(indexpath))
 		if authenticate(request, request.POST["token"]):
-			return HttpResponseRedirect('pyha/index')
+			return HttpResponseRedirect(indexpath)
 		else:
 			return HttpResponseRedirect(settings.LAJIAUTH_URL+'login?target='+settings.TARGET+'&next='+str(indexpath))
 
