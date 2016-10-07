3a4744926039b8808ab06d8c8cc67cc593456da6
Vornanen Riku H <rivorivo@dat-14.cs.helsinki.fi>
2016-10-07 12:40:11 +0300

    mockrequesttestvalmis

--- luomuspyha/pyha/selenium_tests.py
+++ luomuspyha/pyha/selenium_tests.py
@@ -14,7 +14,7 @@ class Mockrequest_TestCase(unittest.TestCase):
 		driver = self.driver
 		driver.get("http://127.0.0.1:8000/mock/jsonmock")
 		driver.find_element_by_xpath("/html/body/form[1]").submit()
-		driver.get("https://fmnh-ws-test.it.helsinki.fi/pyha/")
+		driver.get("http://127.0.0.1:8000/pyha/")
 		driver.find_element_by_id("local-login").click()
 		elem = driver.find_element_by_id("login-form")
 		email = driver.find_element_by_name("email")
@@ -23,7 +23,7 @@ class Mockrequest_TestCase(unittest.TestCase):
 		password.send_keys("L4ausesuomeksi")
 		elem.submit()
 		teksti = driver.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[1]/a[1]')
-		assertEqual('HBF.C60AB314-43E9-41F8-BB7D-0775773B16BD',teksti.text)
+		self.assertEqual('HBF.C60AB314-43E9-41F8-BB7D-0775773B16BD',teksti.text)
 	
 	def tearDown(self):
 		self.driver.close()
