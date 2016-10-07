9b756f73d95a2b569b8e45db64f51de32d7cc075
Vornanen Riku H <rivorivo@dat-14.cs.helsinki.fi>
2016-10-07 13:24:41 +0300

    views eivoi enää samaa kyselyä toimii

--- luomuspyha/pyha/views.py
+++ luomuspyha/pyha/views.py
@@ -19,7 +19,7 @@ def index(request):
 		if not logged_in(request):
 			return _process_auth_response(request,'')
 		userEmail = request.session["user_email"]
-		request_list = Request.requests.filter(email=userEmail)
+		request_list = Request.requests.filter(email=userEmail).order_by('order').reverse
 		context = {"title": "Tervetuloa " + request.session["user_name"] , "message": u"Käytät sähköpostiosoitetta: " + request.session["user_email"], "requests": request_list }
 		return render(request, 'pyha/index.html', context)
 
