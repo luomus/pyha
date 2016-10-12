6f0219febd78d28bb6436c150d64e9f4bfa6ee41
Vornanen Riku H <rivorivo@dat-8.cs.helsinki.fi>
2016-10-12 17:23:54 +0300

    seletestej etusivulle

--- luomuspyha/pyha/selenium_tests.py
+++ luomuspyha/pyha/selenium_tests.py
@@ -9,8 +9,6 @@ class Mockrequest_TestCase(unittest.TestCase):
 		chromedriver = "/home/ad/fshome5/u5/r/rivorivo/Linux/Python/envi3/chromedriver"
 		os.environ["webdriver.chrome.driver"] = chromedriver
 		self.driver = webdriver.Chrome(chromedriver)
-
-	def test_request_link_visible(self):	
 		driver = self.driver
 		driver.get("http://127.0.0.1:8000/mock/jsonmock")
 		driver.find_element_by_xpath("/html/body/form[1]").submit()
@@ -22,9 +20,22 @@ class Mockrequest_TestCase(unittest.TestCase):
 		email.send_keys("pyhatestaaja@gmail.com")
 		password.send_keys("L4ausesuomeksi")
 		elem.submit()
-		teksti = driver.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[1]/a[1]')
-		self.assertEqual('HBF.C60AB314-43E9-41F8-BB7D-0775773B16BD',teksti.text)
+
+	def test_request_link_visible(self):	
+		driver = self.driver
+		teksti = driver.find_element_by_xpath('//table[1]/tbody[1]/tr[1]/td[1]')
+		self.assertEqual('kuvaus',teksti.text)
 	
+	def test_status_sana_ei_numero(self):
+		driver = self.driver
+		teksti = driver.find_element_by_xpath('//table[1]/tbody[1]/tr[1]/td[4]')
+		self.assertEqual('Käsittelyssä',teksti.text)
+	
+	def test_paivaus_fiksusti(self):
+		driver = self.driver
+		teksti = driver.find_element_by_xpath('//table[1]/tbody[1]/tr[1]/td[2]')
+		self.assertEqual('10. lokakuuta 2016 kello 16.22',teksti.text)
+		
 	def tearDown(self):
 		self.driver.close()
 
