4e9e14a469099884db85ea6ac8dd2a1535f3eb59
aalri <kinkku@example.com>
2017-02-20 05:54:44 +0200

    paivitys

--- luomuspyha/static/pyha/js/map.js
+++ luomuspyha/static/pyha/js/map.js
@@ -10,7 +10,7 @@ function initializeMap() {
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 7, attribution: osmAttrib});		
+	var osm = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 14, attribution: osmAttrib});		
 
 	map.setView(new L.LatLng(62.14, 25.44),40);
 	map.addLayer(osm);
