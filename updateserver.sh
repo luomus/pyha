a466694fc0963b3b56ff8e2b4393f2be981747b9
Riku <riku.alho@helsinki.fi>
2018-09-22 19:14:43 -0700

    paivitys

--- updateserver.sh
+++ updateserver.sh
@@ -10,6 +10,10 @@ then
     env/bin/python project/manage.py makemigrations
     env/bin/python project/manage.py migrate
     env/bin/python project/manage.py createcachetable
+    cd project/pyha
+    django-admin makemessages -a
+    django-admin compilemessages
+    cd ../..
 
 else
     echo "Unable to find 'env_variables.sh'. Cannot start"
