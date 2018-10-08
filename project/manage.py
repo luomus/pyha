56dc8ffb1d7bcab11417a50769bafc1451e8086e
aalri <riku.alho@kolumbus.fi>
2018-10-09 02:53:43 +0300

    paivitys

--- project/manage.py
+++ project/manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if "test" in sys.argv:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.test")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.test")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.default")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
