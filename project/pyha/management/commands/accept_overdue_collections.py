26d77ffc0509c43b652d2486e0f1b2f9389763c0
aalri <riku.alho@kolumbus.fi>
2018-12-03 08:20:51 +0200

    paivitys

--- project/pyha/management/commands/accept_overdue_collections.py
+++ project/pyha/management/commands/accept_overdue_collections.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from pyha.database import get_collections_waiting_atleast_days, is_collection_waiting, update_request_status
+from pyha.database import get_collections_waiting_atleast_days, is_collection_waiting_atleast_days, update_request_status
 from pyha.models import Col_StatusEnum, RequestLogEntry, Request
 from pyha.roles import CAT_ADMIN
 from pyha.log_utils import changed_by
@@ -15,6 +15,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         days = 21
         interval = 32400
+        
         overdue = list(get_collections_waiting_atleast_days(days))
                 
         for collection in overdue:
@@ -24,13 +25,13 @@ class Command(BaseCommand):
         
         last = 0
         for collection in overdue:
-            accept_in_delay(collection, collection.performtime - last)
+            accept_in_delay(days, collection, collection.performtime - last)
             last = collection.performtime
 
 
-def accept_in_delay(collection, interval):
+def accept_in_delay(days, collection, interval):
         sleep(randint(0, interval))
-        if(is_collection_waiting(collection)):
+        if(is_collection_waiting_atleast_days(days, collection)):
             collection.status = Col_StatusEnum.APPROVED
             collection.changedBy = changed_by("pyha")
             collection.save()
