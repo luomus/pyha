9784dad84f9afe2c7027070d9b7b8b27774c6826
aorin <meeri.rannisto@gmail.com>
2019-01-08 14:47:11 +0200

    update gunicorn, add project folder to the path

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.main")
 
 application = get_wsgi_application()
