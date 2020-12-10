f515d62dd3171085ba900e8f91bea46b79860d29
aorin <meeri.rannisto@gmail.com>
2020-12-10 14:32:09 +0200

    start working on new status columns for handler and admin

--- project/pyha/views/index.py
+++ project/pyha/views/index.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from pyha.database import handler_mul_req_waiting_for_me_status, handler_mul_information_chat_answered_status, get_mul_all_secured, handlers_cannot_be_updated, is_downloadable, remove_request
+from pyha.database import handler_mul_req_waiting_for_me_status, handler_mul_information_chat_answered_status, get_mul_all_secured, handlers_cannot_be_updated, is_downloadable, remove_request, information_status
 from pyha.localization import check_language
 from pyha.login import logged_in, _process_auth_response, is_request_owner, is_admin
 from pyha.models import Request, Collection, RequestLogEntry, StatusEnum
@@ -31,37 +31,41 @@ def index(http_request):
         toast = http_request.session["toast"]
         http_request.session["toast"] = None
         http_request.session.save()
-    if ADMIN in http_request.session.get("current_user_role", [None]):
-        request_list = Request.objects.filter(status__gt=0).order_by('-date')
+
+    request_list = _get_request_list(http_request, userId)
+    for r in request_list:
+        if(r.status == StatusEnum.DOWNLOADABLE):
+            r.downloadable = is_downloadable(http_request, r)
+
+    current_roles = http_request.session.get("current_user_role", [None])
+    if ADMIN in current_roles or HANDLER_ANY in current_roles:
         get_mul_all_secured(request_list)
         for r in request_list:
-            if(r.status == StatusEnum.DOWNLOADABLE):
-                r.downloadable = is_downloadable(http_request, r)
+            r.information_status = information_status(http_request, r)
             r.email = fetch_email_address(r.user)
+
+    if HANDLER_ANY in current_roles:
+        handler_mul_information_chat_answered_status(request_list, http_request, userId)
+        handler_mul_req_waiting_for_me_status(request_list, http_request, userId)
+        viewedlist = list(RequestLogEntry.requestLog.filter(request__in = [re.id for re in request_list], user = userId, action = 'VIEW'))
+        for r in request_list:
+            if([re.request.id for re in viewedlist].count(r.id) > 0 or not r.status == StatusEnum.WAITING):
+                r.viewed = True
+
+    context = {"role": http_request.session.get("current_user_role", USER), "toast": toast, "username": http_request.session["user_name"], "requests": request_list, "static": settings.STA_URL }
+    return render(http_request, 'pyha/base/index.html', context)
+
+def _get_request_list(http_request, userId):
+    if ADMIN in http_request.session.get("current_user_role", [None]):
+        return Request.objects.filter(status__gt=0).order_by('-date')
     elif HANDLER_ANY in http_request.session.get("current_user_role", [None]):
         request_list = []
         if CAT_HANDLER_COLL in http_request.session.get("user_roles", [None]):
             q = Request.objects.exclude(status__lte=0)
             request_list = q.filter(id__in=Collection.objects.filter(address__in = get_collections_where_download_handler(userId), status__gt = 0 ).values("request"))
-        request_list = sorted(request_list ,key=attrgetter('date'), reverse=True)
-        handler_mul_information_chat_answered_status(request_list, http_request, userId)
-        handler_mul_req_waiting_for_me_status(request_list, http_request, userId)
-        viewedlist = list(RequestLogEntry.requestLog.filter(request__in = [re.id for re in request_list], user = userId, action = 'VIEW'))
-        get_mul_all_secured(request_list)
-        for r in request_list:
-            if(r.status == StatusEnum.DOWNLOADABLE):
-                r.downloadable = is_downloadable(http_request, r)
-            r.email = fetch_email_address(r.user)
-            if([re.request.id for re in viewedlist].count(r.id) > 0 or not r.status == StatusEnum.WAITING):
-                r.viewed = True
+        return sorted(request_list ,key=attrgetter('date'), reverse=True)
     else:
-        request_list = Request.objects.filter(user=userId, status__gte=0).order_by('-date')
-        for r in request_list:
-            if(r.status == StatusEnum.DOWNLOADABLE):
-                r.downloadable = is_downloadable(http_request, r)
-
-    context = {"role": http_request.session.get("current_user_role", USER), "toast": toast, "username": http_request.session["user_name"], "requests": request_list, "static": settings.STA_URL }
-    return render(http_request, 'pyha/base/index.html', context)
+        return Request.objects.filter(user=userId, status__gte=0).order_by('-date')
 
 def group_delete_request(http_request):
     nexturl = http_request.POST.get('next', '/')
