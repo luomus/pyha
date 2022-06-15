a9745a5d15bdd3d1c9a56de5986517542d5a996f
aorin <meeri.rannisto@gmail.com>
2022-06-15 16:09:42 +0300

    continue with crowdin update script

--- crowdin-update.sh
+++ crowdin-update.sh
@@ -6,9 +6,11 @@ echo "Fetching the latest crowdin-cli"
 docker pull crowdin/cli >/dev/null 2>&1
 
 echo "Sending sources to crowdin"
-docker run --rm -v ${SCRIPT_PATH}/crowdin.yml -v ${SCRIPT_PATH}/pyha/locale crowdin/cli crowdin upload sources
+docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale crowdin/cli \
+ crowdin upload sources -c /crowdin.yml
 
 echo "Fetching translations from crowdin"
-docker run --rm -v ${SCRIPT_PATH}/crowdin.yml -v ${SCRIPT_PATH}/pyha/locale crowdin/cli crowdin download
+docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale crowdin/cli \
+ crowdin download -c /crowdin.yml
 
 echo "All done"
