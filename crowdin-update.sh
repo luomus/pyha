eac2f714198866261e549fadba45cf6bd96e4ed7
aorin <meeri.rannisto@gmail.com>
2022-08-24 16:15:21 +0300

    email text files are also send to crowdin

--- crowdin-update.sh
+++ crowdin-update.sh
@@ -6,11 +6,17 @@ echo "Fetching the latest crowdin-cli"
 docker pull crowdin/cli >/dev/null 2>&1
 
 echo "Sending sources to crowdin"
-docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale crowdin/cli \
- crowdin upload sources -c /crowdin.yml
+docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml \
+ -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale \
+ -v ${SCRIPT_PATH}/project/pyha/templates/pyha/email:/project/pyha/templates/pyha/email \
+ --env-file ${SCRIPT_PATH}/.crowdin-env \
+ crowdin/cli crowdin upload sources -c /crowdin.yml
 
 echo "Fetching translations from crowdin"
-docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale crowdin/cli \
- crowdin download -c /crowdin.yml
+docker run --rm -v ${SCRIPT_PATH}/crowdin.yml:/crowdin.yml \
+ -v ${SCRIPT_PATH}/project/pyha/locale:/project/pyha/locale \
+ -v ${SCRIPT_PATH}/project/pyha/templates/pyha/email:/project/pyha/templates/pyha/email \
+ --env-file ${SCRIPT_PATH}/.crowdin-env \
+ crowdin/cli crowdin download -c /crowdin.yml
 
 echo "All done"
